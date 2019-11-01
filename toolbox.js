a4eee1f4255cf57c211189c34e7edafa09cfa230
CaryACTvF <31970190+CaryACTvF@users.noreply.github.com>
2019-11-01 06:26:43 -0400

    Add files via upload

---
+++ toolbox.js
@@ -0,0 +1,28 @@
+// For outputting text in a popup.
+alert("The output goes here!");
+
+// For asking questions in a dialog box.
+let variable_name = prompt("Your question goes here!");
+
+// For assiging a variable:
+let variable_name = "value";
+
+// For selecting an element:
+let variable_name = document.querySelector("CSS Selector");
+
+// For changing innerHTML:
+element.innerHTML = "new value";
+
+// For finding the value of an input element:
+let variable_name = element.value;
+
+// For playing an audio element;
+sound.currentTime = 0;
+sound.play();
+
+// For creating an event listener
+element.addEventListener("event_type", function() {
+	// Your procedure goes here.
+
+});
+
