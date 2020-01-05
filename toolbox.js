245c12b1a0dffe3f1c702a2eb8b4966418f65f45
Cary <cary.e.riina@gmail.com>
2020-01-05 15:26:43 -0500

    updated linked script to refer to updated version

--- toolbox.js
+++ toolbox.js
@@ -62,7 +62,7 @@ String(thing_to_make_a_string);
 // To use any of the functions below this line, 
 // you must copy and paste the following line of code into your HTML file above your script tags
 // and then delete the comment slashes "//".
-// <script type="text/javascript" src="http://cdn.jsdelivr.net/gh/CaryACTvF/toolbox@da2f010/aptools.js"></script>
+// <script type="text/javascript" src="http://cdn.jsdelivr.net/gh/CaryACTvF/toolbox@a244884/aptools.js"></script>
 
 // Repeat Loop:
 // The loop below will repeat 5 times, change the number as you see fit!
@@ -84,7 +84,7 @@ display("message to be displayed")
 
 // Array tools. To use these tools, you must load in the AP Tools
 // using the line of code below:
-// <script type="text/javascript" src="http://cdn.jsdelivr.net/gh/CaryACTvF/toolbox@da2f010/aptools.js"></script>
+// <script type="text/javascript" src="http://cdn.jsdelivr.net/gh/CaryACTvF/toolbox@a244884/aptools.js"></script>
 
 // Starting index for all tools is 1.
 append(list, value);
