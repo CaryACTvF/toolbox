c091356d22212c37e6242bc661f4f80a2c6bae2c
CaryACTvF <31970190+CaryACTvF@users.noreply.github.com>
2022-10-17 06:34:51 -0400

    Create toolbox2022.js

---
+++ toolbox2022.js
@@ -0,0 +1,17 @@
+// For outputting text in a popup.
+alert("The output goes here!");
+
+// For asking questions in a dialog box.
+let variableName = prompt("Your question goes here!");
+
+// For assigning a variable:
+let variableName = "value";
+
+// For selecting an element:
+let variableName = document.querySelector("CSS Selector");
+
+// For creating an event listener
+element.addEventListener("event_type", function() {
+	// Your procedure goes here.
+
+});
