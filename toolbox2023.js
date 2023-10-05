617ae85487ef77234a54ad6463c4297cdf3aea8b
CaryACTvF <31970190+CaryACTvF@users.noreply.github.com>
2023-10-05 08:26:12 -0400

    Create toolbox2023.js

---
+++ toolbox2023.js
@@ -0,0 +1,19 @@
+// For outputting text in a popup.
+alert("The output goes here!");
+
+// For asking questions in a dialog box.
+let variableName = prompt("Your question goes here!");
+
+// For assigning a variable:
+let variableName = "value";
+
+// To get an HTML element so that it can be used in JavaScript:
+let variableName = document.getElementById("ID of element");
+
+// For creating an event listener
+element.addEventListener("event_type", function () {
+	// Your procedure goes here.
+});
+
+// Changing Text on Your Webpage, inside of any element.
+element.innerHTML = "Words go here...";
